fb338cdc2f457880039a94c365b1efc457c9fcad
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-11-15 15:30:08 -0600

    Add files via upload

---
+++ proyects.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+	float division;
+	division= ((1/3)+(3/5)+(1/30))/(23/30);
+	printf("El resultado de la division es:%f\n",division);
+	
+	system("Pause");
+	return 0;
+}
