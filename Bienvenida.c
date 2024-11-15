f1cbf85f091b197c1cd77f5410a0b945b69b179b
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-11-14 21:29:15 -0600

    Add files via upload

---
+++ Bienvenida.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	printf("\a Bienvenido ");
+	printf("es un gusto compilar para ti ");
+	printf ("chinguetas");
+	printf("\n");
+	
+	return NULL;
+
+}
