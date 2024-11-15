bab88fceba634fdf35d262bad475f1d5f360d1c6
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-11-14 21:46:50 -0600

    Add files via upload

---
+++ Declarar 3 variables.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    int b = 10;
+    int c = 3;
+    int resultado = a * b * c;
+    printf("El resultado de la multiplicación de %d x %d x %d es: %d\n", a, b, c, resultado);
+    return 0;
+}
