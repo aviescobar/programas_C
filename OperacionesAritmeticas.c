b75c01e24b786a9fda76f12279617ea3e3eedf9b
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-11-15 15:25:41 -0600

    Add files via upload

---
+++ OperacionesAritmeticas.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    int a = 5;
+    int b = 8;
+    int c = 78787;
+    int d = 3259;
+    int sum1 = a + b;
+    int sum2 = c + d;
+    printf("La suma de 5 y 8 es: %d\n", sum1);
+    printf("La suma de 78787 y 3259 es: %d\n", sum2);
+    return 0;
+}
