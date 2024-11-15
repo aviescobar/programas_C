5c44fc1ddadb6a647a7ca2e8de5a569271467b84
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-11-15 15:30:32 -0600

    Add files via upload

---
+++ prueba.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    double a = 3.1415;
+    double b = 2.7182;
+    double resultado = a / b;
+    printf("El resultado de dividir %lf entre %lf es: %lf\n", a, b, resultado);
+    return 0;
+}
