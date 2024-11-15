d505dd6e6d3d20a8a4a40f3ff6639ea2e8476b7b
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-11-15 15:17:03 -0600

    Add files via upload

---
+++ fraccion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    //expresion a)
+    float a= 1.0/3 + 3.0/5 + (1.0/30) / (23.0/30);
+    printf ("Resultado de la expresion a) %f\n",a);
+//expresion b)
+    float b= 5+2.0/1 + 2.0/2 - 1.0/4;
+    printf("Resultado de la expresion b) %f\n",b);
+
+    return 0;
+}
