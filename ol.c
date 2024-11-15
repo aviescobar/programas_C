810f6e88f1a5d32a18ad86370dafe26356400b6f
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-11-15 15:24:17 -0600

    Add files via upload

---
+++ ol.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int vector [10];
+vector [0] = 1;
+vector [1] = 2;
+vector [2] = 3;
+vector [3] = 4;
+vector [5] = 5;
+vector [6] = 6;
+vector [7] = 7;
+vector [8] = 8;
+vector [9] = 9;
+vector [10] = 10;
+
+printf ("%d", vector [0]);
+printf ("%d", vector [1]);
+printf ("%d", vector [2]);
+printf ("%d", vector [3]);
+printf ("%d", vector [4]);
+printf ("%d", vector [5]);
+printf ("%d", vector [6]);
+printf ("%d", vector [7]);
+printf ("%d", vector [8]);
+printf ("%d", vector [9]);
+printf ("%d", vector [10]);
