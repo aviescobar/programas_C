242c124345fe1c863a8cf6bea8e1195bd0b1c263
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-11-15 15:27:42 -0600

    Add files via upload

---
+++ pop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int numero = 55;
+    if (numero %2 ==){
+        printf("El numero es par\n");
+
+    }
+    printf ("Fin del porgrama \n");
+    system ("Pause");
+    return 0;
+}
