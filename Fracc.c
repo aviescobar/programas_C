2157c2b50642dab648e762adccb3f3a7535b6f64
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-11-15 14:36:16 -0600

    Add files via upload

---
+++ Fracc.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    double resultado = (1.0/3.0 + 3.0/5.0 + 1.0/30.0) / (23.0/30.0 + 0.2229);
+    double resultadob = 5 + (2.0) / (1.0 / (((2.0) / 2.0 - 1.0 / 4.0) + 1.39));
+
+    printf("El resultado del a es: %f", resultado);
+
+    printf("\n");
+
+    printf("El resultado del b es %f", resultadob);
+
+
+    printf("\n");
+    system("Pause");
+    return 0;
+}
