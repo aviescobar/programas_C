5def0906f187909fa1d536f2176b8fdcdb5588c5
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-11-16 21:15:20 -0600

    Add files via upload

---
+++ studio.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int longitudSerie =50;
+    int i;
+
+    for (i - 1; i<=(longitudSerie/2); i++){
+        printf("%d," , 2*1);
+        printf("%d,", 3*1);
+    }
+
+}
