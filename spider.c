3d749f6b60ec5c0e00fb0d23d72edf9f02907c24
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-11-16 21:11:57 -0600

    Add files via upload

---
+++ spider.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int numero = 1;
+
+    while ( numero <= 100000000){
+
+        printf ("%d, " , numero);
+        numero ++;
+
+    } 
+     printf ("Fin del programa" "\a");
+    system("Pause");
+    return 0;
+
+} //Fin main
